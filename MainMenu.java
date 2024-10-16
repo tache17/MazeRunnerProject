fba856a7628d43f4aa2e5ee38bc5334c523fb695
tache17 <vascoluis17@gmail.com>
2024-10-16 18:53:26 +0200

    Commit message

---
+++ MainMenu.java
@@ -0,0 +1,75 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.*;
+
+
+public class MainMenu extends JPanel implements ActionListener, MouseListener{
+    JFrame frame;
+    JButton startButton;
+    JLabel label;
+    GridBagConstraints gbc = new GridBagConstraints();
+
+
+    MainMenu(){
+        // Frame, Panel, Button and Label SET UP
+        ImageIcon icon = new ImageIcon("MazeRunnerProject\\MazeRunnerLogo.png");
+        frame = new JFrame("Main Menu");
+        label = new JLabel(icon);
+        frame.add(this);
+        startButton = new JButton("Start!");
+
+        // Designing
+        setBackground(Color.GRAY);
+        setLayout(new GridBagLayout());
+        startButton.addActionListener(this);
+        startButton.addMouseListener(this);
+        startButton.setFocusable(false);
+        startButton.setPreferredSize(new Dimension(100, 100));
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        this.add(startButton, gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        this.add(label, gbc);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1600, 1000);
+        frame.setVisible(true);
+
+    }
+
+    public void actionPerformed(ActionEvent e){
+
+        new MazeRunnerGame();
+        frame.dispose();
+
+
+    }
+
+    public void mouseClicked(MouseEvent e){
+
+    }
+
+    public void mousePressed(MouseEvent e){
+        
+    }
+
+    public void mouseReleased(MouseEvent e){
+
+    }
+
+    public void mouseEntered(MouseEvent e){
+        startButton.setBackground(Color.YELLOW);
+    }
+
+    public void mouseExited(MouseEvent e){
+        startButton.setBackground(Color.WHITE);
+    }
+
+    public static void main(String[] args){
+        new MainMenu();
+    }
+}
---
+++ MazeRunnerGame.java
@@ -0,0 +1,77 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.*;
+
+
+public class MazeRunnerGame implements MouseListener, ActionListener {
+    JFrame frame;
+    JPanel first;
+    JPanel second;
+    JPanel third;
+
+
+
+    MazeRunnerGame(){
+        // Initiate components
+        JFrame frame = new JFrame("MazeRunner");
+        frame.setLayout(new GridLayout(1, 3));
+        JPanel first = new JPanel();
+        JPanel second = new JPanel();
+        JPanel third = new JPanel();
+
+        frame.add(first);
+        frame.add(second);
+        frame.add(third);
+
+
+
+
+
+
+
+        // Design
+        first.setBackground(Color.RED);
+        second.setBackground(Color.GREEN);       
+        third.setBackground(Color.BLUE);
+
+
+        frame.add(first);
+
+        frame.add(second);
+        frame.add(third);
+
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1600, 1000);
+        frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        new MazeRunnerGame();
+
+    }
+
+    public void mouseClicked(MouseEvent e){
+
+    }
+
+    public void mousePressed(MouseEvent e){
+        
+    }
+
+    public void mouseReleased(MouseEvent e){
+
+    }
+
+    public void mouseEntered(MouseEvent e){
+
+    }
+
+    public void mouseExited(MouseEvent e){
+
+    }
+
+}
